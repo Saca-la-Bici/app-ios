aaf47ed9c1c09a7b5427da9fcbfc5fca516196e7
Jesus0204 <a01705442@tec.mx>
2024-09-17 11:31:32 -0600

    docs(Architecture): crear app en xcode

---
+++ Saca la Bici/Saca_la_BiciApp.swift
@@ -0,0 +1,17 @@
+//
+//  Saca_la_BiciApp.swift
+//  Saca la Bici
+//
+//  Created by Jesus Cedillo on 17/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Saca_la_BiciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Saca la BiciTests/Saca_la_BiciTests.swift
@@ -0,0 +1,17 @@
+//
+//  Saca_la_BiciTests.swift
+//  Saca la BiciTests
+//
+//  Created by Jesus Cedillo on 17/09/24.
+//
+
+import Testing
+@testable import Saca_la_Bici
+
+struct Saca_la_BiciTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
