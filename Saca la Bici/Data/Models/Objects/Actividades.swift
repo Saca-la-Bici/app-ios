2c635463d9317eb00232fee471f52c3027a607ac
MariaJoseGaytan <A01706616@tec.mx>
2024-09-29 13:37:07 -0600

    fix: modelo actividades

---
+++ Saca la Bici/Data/Models/Objects/Actividades.swift
@@ -0,0 +1,75 @@
+//
+//  Actividades.swift
+//  Saca la Bici
+//
+//  Created by Maria Jose Gaytan Gil on 26/09/24.
+//
+
+import Foundation
+
+struct RodadasApiResponse: Codable {
+    let rodadas: [RodadasResponse]
+    let rol: String
+}
+
+struct EventosApiResponse: Codable {
+    let eventos: [EventosResponse]
+    let rol: String
+}
+
+struct TalleresApiResponse: Codable {
+    let talleres: [TalleresResponse]
+    let rol: String
+}
+
+struct RodadasResponse: Codable {
+    let _id: String
+    let informacion: [Actividad]
+    let ruta: Ruta
+}
+
+struct EventosResponse: Codable {
+    let _id: String
+    let informacion: [Actividad]
+}
+
+struct TalleresResponse: Codable {
+    let _id: String
+    let informacion: [Actividad]
+}
+
+struct Actividad: Codable, Identifiable {
+    let _id: String
+    let titulo: String
+    let fecha: String
+    let hora: String
+    let personasInscritas: Int
+    let ubicacion: String
+    let descripcion: String
+    let estado: Bool
+    let duracion: String
+    let imagen: String?
+    let tipo: String
+    let comentarios: String?
+    
+    var id: String { _id }
+}
+
+struct Ruta: Codable {
+    let _id: String
+    let titulo: String
+    let distancia: String
+    let tiempo: String
+    let nivel: String
+    let coordenadas: [Coordenada]
+    let __v: Int
+}
+
+struct Coordenada: Codable, Identifiable {
+    let latitud: Double
+    let longitud: Double
+    let tipo: String
+    let _id: String
+    
+    var id: String { _id }
+}
