962ddd2477ced3ff84cfff817b8ab0184c8cf39f
MariaJoseGaytan <A01706616@tec.mx>
2024-10-03 20:16:57 -0600

    fix: usar el componente para imagenes

--- Saca la Bici/Framework/Views/Anuncios/AnadirAnuncioView.swift
+++ Saca la Bici/Framework/Views/Anuncios/AnadirAnuncioView.swift
@@ -77,55 +77,8 @@ struct AnadirAnuncioView: View {
     
             Spacer().frame(height: 40)
 
-            // Icono para subir una imagen
-            if selectedImageData == nil {
-                PhotosPicker(selection: $selectedItem, matching: .images, photoLibrary: .shared()) {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 25)
-                            .fill(Color.gray.opacity(0.2))
-                            .frame(width: 100, height: 100)
-                        
-                        Image(systemName: "photo.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 60, height: 60)
-                            .foregroundColor(Color.gray.opacity(0.1))
-                        
-                        Image(systemName: "plus")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 35, height: 35)
-                            .foregroundColor(Color.white.opacity(0.7))
-                    }
-                }
-                .onChange(of: selectedItem) { _, newItem in
-                    Task {
-                        if let data = try? await newItem?.loadTransferable(type: Data.self),
-                           UIImage(data: data) != nil {
-                            selectedImageData = data
-                        }
-                    }
-                }
-            }
-
-            // Mostrar la imagen si existe
-            if let data = selectedImageData, let uiImage = UIImage(data: data) {
-                VStack {
-                    Image(uiImage: uiImage)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 200, height: 200)
-                        .cornerRadius(10)
-                    
-                    Button(action: {
-                        selectedImageData = nil
-                    }, label: {
-                        Text("Eliminar Imagen")
-                            .foregroundColor(.red)
-                            .padding(.top, 5)
-                    })
-                }
-            }
+            // Componenete del picker
+            ImagePickerView(selectedItem: $selectedItem, selectedImageData: $selectedImageData)
 
             Spacer().frame(height: 20)
             
