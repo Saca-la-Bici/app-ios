91c055281bf7bbc04761aeaf32948744734d2f5c
Jesus0204 <a01705442@tec.mx>
2024-10-08 22:54:13 -0600

    refactor: mostrar loadingView mientras cargan dato y por si hay algún error

--- Saca la Bici/Framework/Views/Profile/ProfileView.swift
+++ Saca la Bici/Framework/Views/Profile/ProfileView.swift
@@ -44,126 +44,132 @@ struct ProfileView: View {
                         .bold()
                 }
                 
-                ScrollView {
-                    
-                    Spacer().frame(height: 10)
-                    
-                    // Imagen de perfil con borde de color
-                    VStack {
-                        HStack {
-                            // Elementos invisibles para alinear el contenido
-                            HStack(spacing: 3) {
-                                Image("BloodDrop")
-                                    .resizable()
-                                    .frame(width: 18, height: 20)
-                                    .padding(.top, 30)
-                                    .opacity(0)
-                                Text(consultarPerfilPropioViewModel.profile?.tipoSangre ?? "")
-                                    .font(.subheadline)
-                                    .padding(.top, 30)
-                                    .opacity(0)
-                            }
-                            // Imagen centrada
-                            if let imageUrlString = consultarPerfilPropioViewModel.profile?.imagen, let imageUrl = URL(string: imageUrlString) {
-                                AsyncImage(url: imageUrl) { phase in
-                                    switch phase {
-                                    case .empty:
-                                        ProgressView()
-                                            .frame(width: 80, height: 80)
-                                            .background(Color.gray.opacity(0.1))
-                                            .clipShape(Circle())
-                                    case .success(let image):
-                                        image
-                                            .resizable()
-                                            .frame(width: 80, height: 80)
-                                            .clipShape(Circle())
-                                            .overlay(Circle().stroke(Color.black, lineWidth: 1))
-                                            .shadow(color: .gray, radius: 2, x: 2, y: 2)
-                                    case .failure:
-                                        Image(systemName: "person.crop.circle.badge.exclamationmark")
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(width: 80, height: 80)
-                                            .foregroundColor(.gray)
-                                            .clipShape(Circle())
-                                            .overlay(Circle().stroke(Color.black, lineWidth: 1))
-                                            .shadow(color: .gray, radius: 2, x: 2, y: 2)
-                                    @unknown default:
-                                        EmptyView()
-                                    }
-                                }
-                            } else {
-                                Image(systemName: "person.crop.circle.fill")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 80, height: 80)
-                                    .foregroundColor(.gray)
-                                    .clipShape(Circle())
-                                    .overlay(Circle().stroke(Color.black, lineWidth: 1))
-                                    .shadow(color: .gray, radius: 2, x: 2, y: 2)
-                            }
-                            
-                            // Elementos visibles
-                            HStack(spacing: 3) {
-                                Image("BloodDrop")
-                                    .resizable()
-                                    .frame(width: 18, height: 20)
-                                    .padding(.top, 50)
-                                
-                                Text(consultarPerfilPropioViewModel.profile?.tipoSangre ?? "")
-                                    .font(.subheadline)
-                                    .padding(.top, 50)
-                            }
-                        }
-                        .frame(maxWidth: .infinity)
+                if consultarPerfilPropioViewModel.isLoading == true {
+                    Spacer()
+                    ProgressView("Cargando perfil...")
+                } else {
+                    ScrollView {
                         
-                        Text(consultarPerfilPropioViewModel.profile?.nombre ?? "")
-                            .font(.subheadline)
-                    }
-                    .padding(.bottom, 10)
-                    
-                    // Estadísticas de usuario: Rodadas, Kilómetros
-                    HStack {
-                        VStack(spacing: 5) {  // Reducimos el espacio entre los elementos dentro del VStack
-                            Text("\(consultarPerfilPropioViewModel.profile?.rodadasCompletadas ?? 1)")
-                                .font(.system(size: 12))
-                            Text("Rodadas")
-                                .font(.system(size: 14))
-                                .fontWeight(.bold)
+                        Spacer().frame(height: 10)
+                        
+                        // Imagen de perfil con borde de color
+                        VStack {
+                            HStack {
+                                // Elementos invisibles para alinear el contenido
+                                HStack(spacing: 3) {
+                                    Image("BloodDrop")
+                                        .resizable()
+                                        .frame(width: 18, height: 20)
+                                        .padding(.top, 30)
+                                        .opacity(0)
+                                    Text(consultarPerfilPropioViewModel.profile?.tipoSangre ?? "")
+                                        .font(.subheadline)
+                                        .padding(.top, 30)
+                                        .opacity(0)
+                                }
+                                // Imagen centrada
+                                if let imageUrlString = consultarPerfilPropioViewModel.profile?.imagen, let imageUrl = URL(string: imageUrlString) {
+                                    AsyncImage(url: imageUrl) { phase in
+                                        switch phase {
+                                        case .empty:
+                                            ProgressView()
+                                                .frame(width: 80, height: 80)
+                                                .background(Color.gray.opacity(0.1))
+                                                .clipShape(Circle())
+                                        case .success(let image):
+                                            image
+                                                .resizable()
+                                                .frame(width: 80, height: 80)
+                                                .clipShape(Circle())
+                                                .overlay(Circle().stroke(Color.black, lineWidth: 1))
+                                                .shadow(color: .gray, radius: 2, x: 2, y: 2)
+                                        case .failure:
+                                            Image(systemName: "person.crop.circle.badge.exclamationmark")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 80, height: 80)
+                                                .foregroundColor(.gray)
+                                                .clipShape(Circle())
+                                                .overlay(Circle().stroke(Color.black, lineWidth: 1))
+                                                .shadow(color: .gray, radius: 2, x: 2, y: 2)
+                                        @unknown default:
+                                            EmptyView()
+                                        }
+                                    }
+                                } else {
+                                    Image(systemName: "person.crop.circle.fill")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 80, height: 80)
+                                        .foregroundColor(.gray)
+                                        .clipShape(Circle())
+                                        .overlay(Circle().stroke(Color.black, lineWidth: 1))
+                                        .shadow(color: .gray, radius: 2, x: 2, y: 2)
+                                }
+                                
+                                // Elementos visibles
+                                HStack(spacing: 3) {
+                                    Image("BloodDrop")
+                                        .resizable()
+                                        .frame(width: 18, height: 20)
+                                        .padding(.top, 50)
+                                    
+                                    Text(consultarPerfilPropioViewModel.profile?.tipoSangre ?? "")
+                                        .font(.subheadline)
+                                        .padding(.top, 50)
+                                }
+                            }
+                            .frame(maxWidth: .infinity)
+                            
+                            Text(consultarPerfilPropioViewModel.profile?.nombre ?? "")
+                                .font(.subheadline)
                         }
-                        Spacer()
-                        VStack(spacing: 5) {  // Reducimos el espacio entre los elementos dentro del VStack
-                            Text("\(String(format: "%.1f", consultarPerfilPropioViewModel.profile?.kilometrosRecorridos ?? 5))km")
-                                .font(.system(size: 12))
-                            Text("Kilómetros")
-                                .font(.system(size: 14))
-                                .fontWeight(.bold)
+                        .padding(.bottom, 10)
+                        
+                        // Estadísticas de usuario: Rodadas, Kilómetros
+                        HStack {
+                            VStack(spacing: 5) {  // Reducimos el espacio entre los elementos dentro del VStack
+                                Text("\(consultarPerfilPropioViewModel.profile?.rodadasCompletadas ?? 1)")
+                                    .font(.system(size: 12))
+                                Text("Rodadas")
+                                    .font(.system(size: 14))
+                                    .fontWeight(.bold)
+                            }
+                            Spacer()
+                            VStack(spacing: 5) {  // Reducimos el espacio entre los elementos dentro del VStack
+                                Text("\(String(format: "%.1f", consultarPerfilPropioViewModel.profile?.kilometrosRecorridos ?? 5))km")
+                                    .font(.system(size: 12))
+                                Text("Kilómetros")
+                                    .font(.system(size: 14))
+                                    .fontWeight(.bold)
+                            }
                         }
-                    }
-                    .padding(.horizontal, 100)  // Ajustamos el padding para que se vea más compacto
-                    .padding(.bottom, 10)
+                        .padding(.horizontal, 100)  // Ajustamos el padding para que se vea más compacto
+                        .padding(.bottom, 10)
 
-                    HStack {
-                        Button {
-                            // Acción para Editar perfil
-                        } label: {
-                            Text("Editar perfil")
-                                .font(.system(size: 14))
-                                .padding(.all, 7)
-                                .frame(maxWidth: .infinity)  // Ajustamos el ancho para que ocupe todo el espacio disponible
-                                .background(Color(red: 243/255, green: 240/255, blue: 235/255))
-                                .foregroundColor(.black)
-                                .cornerRadius(10)
-                                .shadow(radius: 5, y: 3)
+                        HStack {
+                            Button {
+                                // Acción para Editar perfil
+                            } label: {
+                                Text("Editar perfil")
+                                    .font(.system(size: 14))
+                                    .padding(.all, 7)
+                                    .frame(maxWidth: .infinity)  // Ajustamos el ancho para que ocupe todo el espacio disponible
+                                    .background(Color(red: 243/255, green: 240/255, blue: 235/255))
+                                    .foregroundColor(.black)
+                                    .cornerRadius(10)
+                                    .shadow(radius: 5, y: 3)
+                            }
+                            .buttonStyle(PlainButtonStyle())
+                            .frame(maxWidth: .infinity)  // Hacemos que el botón se extienda a lo ancho del HStack
                         }
-                        .buttonStyle(PlainButtonStyle())
-                        .frame(maxWidth: .infinity)  // Hacemos que el botón se extienda a lo ancho del HStack
-                    }
-                    .padding(.horizontal, 100)  // Ajustamos el padding para que el botón esté alineado con los textos de "Rodadas" y "Kilómetros"
-                    .padding(.bottom, 10)
+                        .padding(.horizontal, 100)  // Ajustamos el padding para que el botón esté alineado con los textos de "Rodadas" y "Kilómetros"
+                        .padding(.bottom, 10)
 
-                    IconSelectionView()
+                        IconSelectionView()
+                    }
                 }
+                Spacer()
             }
             .onAppear {
                 Task {
@@ -173,7 +179,7 @@ struct ProfileView: View {
             }
             .alert(isPresented: .constant(consultarPerfilPropioViewModel.errorMessage != nil)) {
                 Alert(
-                    title: Text("Error"),
+                    title: Text("Oops!"),
                     message: Text(consultarPerfilPropioViewModel.errorMessage ?? "Hubo un error al ingresar a tu perfil, intente de nuevo más tarde"),
                     dismissButton: .default(Text("Aceptar"), action: {
                         consultarPerfilPropioViewModel.errorMessage = nil
