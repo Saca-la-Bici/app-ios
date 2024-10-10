b9703a152d9d1cd7817abdf6f151afc28ce97219
Jesus0204 <a01705442@tec.mx>
2024-10-09 23:27:22 -0600

    feat: cargar datos de perfil en vistas de restablecer contraseña y seguridad de la cuenta

--- Saca la Bici/Framework/Views/Profile/ProfileView.swift
+++ Saca la Bici/Framework/Views/Profile/ProfileView.swift
@@ -68,44 +68,7 @@ struct ProfileView: View {
                                         .opacity(0)
                                 }
                                 // Imagen centrada
-                                if let imageUrlString = consultarPerfilPropioViewModel.profile?.imagen, let imageUrl = URL(string: imageUrlString) {
-                                    AsyncImage(url: imageUrl) { phase in
-                                        switch phase {
-                                        case .empty:
-                                            ProgressView()
-                                                .frame(width: 80, height: 80)
-                                                .background(Color.gray.opacity(0.1))
-                                                .clipShape(Circle())
-                                        case .success(let image):
-                                            image
-                                                .resizable()
-                                                .frame(width: 80, height: 80)
-                                                .clipShape(Circle())
-                                                .overlay(Circle().stroke(Color.black, lineWidth: 1))
-                                                .shadow(color: .gray, radius: 2, x: 2, y: 2)
-                                        case .failure:
-                                            Image(systemName: "person.crop.circle.badge.exclamationmark")
-                                                .resizable()
-                                                .scaledToFit()
-                                                .frame(width: 80, height: 80)
-                                                .foregroundColor(.gray)
-                                                .clipShape(Circle())
-                                                .overlay(Circle().stroke(Color.black, lineWidth: 1))
-                                                .shadow(color: .gray, radius: 2, x: 2, y: 2)
-                                        @unknown default:
-                                            EmptyView()
-                                        }
-                                    }
-                                } else {
-                                    Image(systemName: "person.crop.circle.fill")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 80, height: 80)
-                                        .foregroundColor(.gray)
-                                        .clipShape(Circle())
-                                        .overlay(Circle().stroke(Color.black, lineWidth: 1))
-                                        .shadow(color: .gray, radius: 2, x: 2, y: 2)
-                                }
+                                ProfileImageView(imageUrlString: consultarPerfilPropioViewModel.profile?.imagen)
                                 
                                 // Elementos visibles
                                 HStack(spacing: 3) {
