39c7101f403c5c484091997503efde36bed2d546
DiegoGarciaPadilla <gpdiego370@outlook.com>
2024-10-10 08:35:53 -0600

    fix: eliminar referencia y subir archivo

---
+++ Saca la Bici/Domain/Actividades/EliminarActividadRequirement.swift
@@ -0,0 +1,33 @@
+//
+//  EliminarActividadRequirement.swift
+//  Saca la Bici
+//
+//  Created by Diego Antonio García Padilla on 09/10/24.
+//
+
+import Foundation
+
+// Protocolo
+protocol EliminarActividadRequirementProtocol {
+    func eliminarActividad(id: String, tipo: String) async throws -> EliminarActividadResponse
+}
+
+// Requirement
+class EliminarActividadRequirement: EliminarActividadRequirementProtocol {
+    
+    // Singleton
+    static let shared = EliminarActividadRequirement()
+    
+    // Repository
+    let repository: ActividadesRepository
+    
+    init(repository: ActividadesRepository = ActividadesRepository()) {
+        self.repository = repository
+    }
+    
+    func eliminarActividad(id: String, tipo: String) async throws -> EliminarActividadResponse {
+        return try await repository.eliminarActividad(id: id, tipo: tipo)
+    }
+    
+    
+}
