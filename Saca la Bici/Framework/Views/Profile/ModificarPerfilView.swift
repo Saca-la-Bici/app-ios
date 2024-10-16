46ad29689da681f628f24987434aab7eb006585c
Jesus0204 <a01705442@tec.mx>
2024-10-15 22:17:20 -0600

    fix: hacer que al hacer tap fuera se esconda el teclado

--- Saca la Bici/Framework/Views/Profile/ModificarPerfilView.swift
+++ Saca la Bici/Framework/Views/Profile/ModificarPerfilView.swift
@@ -22,58 +22,65 @@ struct ModificarPerfilView: View {
     let tiposSangre = [ "Sin seleccionar", "A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"]
 
     var body: some View {
-        VStack {
-            profileHeader() // Encabezado del perfil con la imagen seleccionada
-            formFields() // Campos del formulario
-        }
-        .navigationTitle("Editar Perfil")
-        .navigationBarTitleDisplayMode(.inline)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                saveButton() // Botón para guardar cambios
-            }
-        }
-        .onAppear {
-            // Inicializa los valores del perfil al aparecer la vista
-            let profile = consultarPerfilPropioViewModel.profile
-            nombre = profile?.nombre ?? ""
-            nombreUsuario = profile?.username ?? ""
-            tipoSangre = profile?.tipoSangre ?? "Sin seleccionar"
-            telefonoEmergencia = profile?.numeroEmergencia ?? ""
-            
-            // Si el perfil tiene una imagen, cargarla en selectedImageData
-            if let imageUrlString = profile?.imagen, let imageUrl = URL(string: imageUrlString) {
-                Task {
-                    do {
-                        let (data, _) = try await URLSession.shared.data(from: imageUrl)
-                        selectedImageData = data
-                    } catch {
-                        print("Error al cargar la imagen: \(error)")
+        ZStack {
+            ScrollView {
+                VStack {
+                    profileHeader() // Encabezado del perfil con la imagen seleccionada
+                    formFields() // Campos del formulario
+                }
+                .navigationTitle("Editar Perfil")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        saveButton() // Botón para guardar cambios
                     }
                 }
-            }
-        }
-        .alert("Resultado", isPresented: $mostrarAlerta) {
-            Button("Aceptar", role: .cancel) {
-                if resultado == "Perfil modificado correctamente" {
-                    path.removeLast() // Solo regresa si el perfil fue modificado exitosamente
-                }
-            }
-        } message: {
-            Text(resultado)
-        }
-        .overlay(
-            Group {
-                if isLoading {
-                    ZStack {
-                        Color.white // Fondo completamente blanco
-                            .ignoresSafeArea() // Para que ocupe toda la pantalla
-                        ProgressView("Mandando información...")
+                .onAppear {
+                    // Inicializa los valores del perfil al aparecer la vista
+                    let profile = consultarPerfilPropioViewModel.profile
+                    nombre = profile?.nombre ?? ""
+                    nombreUsuario = profile?.username ?? ""
+                    tipoSangre = profile?.tipoSangre ?? "Sin seleccionar"
+                    telefonoEmergencia = profile?.numeroEmergencia ?? ""
+                    
+                    // Si el perfil tiene una imagen, cargarla en selectedImageData
+                    if let imageUrlString = profile?.imagen, let imageUrl = URL(string: imageUrlString) {
+                        Task {
+                            do {
+                                let (data, _) = try await URLSession.shared.data(from: imageUrl)
+                                selectedImageData = data
+                            } catch {
+                                print("Error al cargar la imagen: \(error)")
+                            }
+                        }
                     }
                 }
+                .alert("Resultado", isPresented: $mostrarAlerta) {
+                    Button("Aceptar", role: .cancel) {
+                        if resultado == "Perfil modificado correctamente" {
+                            path.removeLast() // Solo regresa si el perfil fue modificado exitosamente
+                        }
+                    }
+                } message: {
+                    Text(resultado)
+                }
+                .overlay(
+                    Group {
+                        if isLoading {
+                            ZStack {
+                                Color.white // Fondo completamente blanco
+                                    .ignoresSafeArea() // Para que ocupe toda la pantalla
+                                ProgressView("Mandando información...")
+                            }
+                        }
+                    }
+                )
             }
-        )
-
+            .onTapGesture {
+                UIApplication.shared.hideKeyboard()
+            }
+            .frame(maxWidth: .infinity)
+        }
     }
 
     @ViewBuilder
@@ -91,41 +98,38 @@ struct ModificarPerfilView: View {
 
     @ViewBuilder
     private func formFields() -> some View {
-        ScrollView {
-            VStack(spacing: 15) {
-                TextoLimiteField(label: "Nombre", placeholder: "Nombre", text: $nombre, maxLength: 30)
-                TextoLimiteField(label: "Nombre de usuario", placeholder: "Nombre de usuario", text: $nombreUsuario, maxLength: 20)
-                VStack(alignment: .leading) {
-                    Text("Teléfono de Emergencia")
-                        .font(.caption)
+        VStack(spacing: 15) {
+            TextoLimiteField(label: "Nombre", placeholder: "Nombre", text: $nombre, maxLength: 40)
+            TextoLimiteField(label: "Nombre de usuario", placeholder: "Nombre de usuario", text: $nombreUsuario, maxLength: 25)
+            VStack(alignment: .leading) {
+                Text("Teléfono de Emergencia")
+                    .font(.caption)
+                
+                HStack {
+                    Text("+")
+                        .font(.title2)
+                        .padding(.leading, 10)
                     
-                    HStack {
-                        Text("+")
-                            .font(.title2)
-                            .padding(.leading, 10)
-                        
-                        TextField("Teléfono de Emergencia", text: $telefonoEmergencia)
-                            .keyboardType(.numberPad)
-                            .padding()
-                            .cornerRadius(10)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.gray.opacity(0.4), lineWidth: 1)
-                            )
-                            .onReceive(Just(telefonoEmergencia)) { _ in
-                                if telefonoEmergencia.count > 15 {
-                                    telefonoEmergencia = String(telefonoEmergencia.prefix(15))
-                                }
+                    TextField("Teléfono de Emergencia", text: $telefonoEmergencia)
+                        .keyboardType(.numberPad)
+                        .padding()
+                        .cornerRadius(10)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.gray.opacity(0.4), lineWidth: 1)
+                        )
+                        .onReceive(Just(telefonoEmergencia)) { _ in
+                            if telefonoEmergencia.count > 15 {
+                                telefonoEmergencia = String(telefonoEmergencia.prefix(15))
                             }
-                    }
-                    
+                        }
                 }
                 
-                TipoSangrePicker(selectedBloodType: $tipoSangre, bloodTypes: tiposSangre)
             }
-            .padding(.horizontal)
+            
+            TipoSangrePicker(selectedBloodType: $tipoSangre, bloodTypes: tiposSangre)
         }
-        .frame(maxWidth: .infinity)
+        .padding(.horizontal)
     }
 
     @ViewBuilder
