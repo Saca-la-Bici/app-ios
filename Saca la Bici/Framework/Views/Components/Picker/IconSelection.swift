fc881c40127f33881c1ab30f0daf0792a8b74faa
Dieg0Lir4 <a01710369@tec.mx>
2024-10-16 07:42:24 -0600

    refactor(scrollview): Quitar el drag para que funcione el scrollview

--- Saca la Bici/Framework/Views/Components/Picker/IconSelection.swift
+++ Saca la Bici/Framework/Views/Components/Picker/IconSelection.swift
@@ -13,25 +13,26 @@ struct IconSelectionView: View {
     @State private var offset: CGSize = .zero
 
     var body: some View {
-        VStack {
-            // Íconos para seleccionar la vista
-            HStack(spacing: 100) {
-                // Ícono de calendario
-                Button {
-                    withAnimation {
-                        selectedIcon = 0
+        NavigationStack(path: $path) {
+            VStack {
+                // Íconos para seleccionar la vista
+                HStack(spacing: 100) {
+                    // Ícono de calendario
+                    Button {
+                        withAnimation {
+                            selectedIcon = 0
+                        }
+                    } label: {
+                        Image(systemName: "calendar")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(selectedIcon == 0 ? .yellow : .primary)
                     }
-                } label: {
-                    Image(systemName: "calendar")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 20, height: 20)
-                        .foregroundColor(selectedIcon == 0 ? .yellow : .primary)
-                }
-                .buttonStyle(PlainButtonStyle())
-
-                // Ícono de palomita (checkmark)
-                Button {
+                    .buttonStyle(PlainButtonStyle())
+                    
+                    // Ícono de palomita (checkmark)
+                    Button {
                         withAnimation {
                             selectedIcon = 1
                         }
@@ -43,9 +44,9 @@ struct IconSelectionView: View {
                             .foregroundColor(selectedIcon == 1 ? .yellow : .primary)
                     }
                     .buttonStyle(PlainButtonStyle())
-
-                // Ícono de mundo
-                Button {
+                    
+                    // Ícono de mundo
+                    Button {
                         withAnimation {
                             selectedIcon = 2
                         }
@@ -57,56 +58,35 @@ struct IconSelectionView: View {
                             .foregroundColor(selectedIcon == 2 ? .yellow : .primary)
                     }
                     .buttonStyle(PlainButtonStyle())
-            }
-            .padding(.bottom, 35)
-            
-            // ZStack para las vistas con animación de transición
-            ZStack {
-                if selectedIcon == 0 {
-                    VStack {
-                        EventView(path: $path)
-                        Spacer()
-                    }
-                    .transition(.scale)
-                    
-                } else if selectedIcon == 1 {
-                    VStack {
-                        MedalsView()
-                        Spacer()
-                    }
-                    .transition(.scale)
-                } else if selectedIcon == 2 {
-                    Text("Vista de mundo")
-                        .font(.title)
-                        .transition(.scale)
-                    Spacer()
                 }
+                .padding(.bottom, 35)
+                
+                // ZStack para las vistas con animación de transición
+                ZStack {
+                    if selectedIcon == 0 {
+                        VStack {
+                            EventView(path: $path)
+                            Spacer()
+                        }
+                        .transition(.scale)
+                        
+                    } else if selectedIcon == 1 {
+                        VStack {
+                            MedalsView()
+                            Spacer()
+                        }
+                        .transition(.scale)
+                    } else if selectedIcon == 2 {
+                        Text("Vista de mundo")
+                            .font(.title)
+                            .transition(.scale)
+                        Spacer()
+                    }
+                }
+                
+                Spacer()
             }
-            .offset(x: offset.width)
-            .gesture(
-                DragGesture()
-                    .onChanged { gesture in
-                        offset = gesture.translation
-                    }
-                    .onEnded { gesture in
-                        if gesture.translation.width < -100 && selectedIcon < 2 {
-                            withAnimation {
-                                selectedIcon += 1
-                            }
-                        } else if gesture.translation.width > 100 && selectedIcon > 0 {
-                            withAnimation {
-                                selectedIcon -= 1
-                            }
-                        }
-                        withAnimation {
-                            offset = .zero
-                        }
-                    }
-            )
-            .animation(.easeInOut, value: selectedIcon)
-
-            Spacer()
+            .padding()
         }
-        .padding()
     }
 }
